4749d5c6983513d312ec959fb73d31e40853622a
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2022-10-25 23:39:49 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// the 4 basic concepts of OOP:
+// - 1. Abstraction: Only present data that is relevant or essential & hiding details within a class/object
+// - 2. Encapsulation: Keeping all attributes and behaviors together within the given scope of a class/object
+// - 3. Inheritance: subclasses (children) inherit all attributes and behaviors of the superclass (parent)
+// - 4. Polymorphism...
+
+// POLYMORPHISM -------------------------------
+// Polymorphism is the ability of an object to take on many forms.
+// examples of this include a subclass overriding its parent methods
+// as it relates to TypeScript, we can describe an object by the parent class even if it was constructed from the child class
+
+// interfaces can also contain methods
+// ...
+
+// interfaces simply describe an object, it will not create one (we have to do that manually)
+// ...
+
+// classes can be described by interfaces using the "implements" keyword (rather than "extends")
+// ...
+
+// we can describe an object by the parent class even if it was constructed from the child class
+// ...
